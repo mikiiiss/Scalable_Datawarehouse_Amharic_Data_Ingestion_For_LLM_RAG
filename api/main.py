d92a41425d52f532086ce4bea8b2b672f5e00225
eyerusalem <eyerusad12@gmail.com>
2024-05-22 12:36:05 +0300

    end point added for create

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
+from sqlalchemy.orm import Session
+from  models.base import SessionLocal, engine
+import view_model.data as data_vm
+import models.data as data_model
 
+app = FastAPI()
 
 
 app.add_middleware(
@@ -13,20 +16,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/data/", response_model=data_vm.DataCreate)
+def create_data(data:data_vm.DataCreate, db: Session = Depends(get_db)):
+    # db_user = crud.get_user_by_email(db, email=user.email)
+    # if db_user:
+    #     raise HTTPException(status_code=400, detail="Email already registered")
+    # return crud.create_user(db=db, user=user)
+    return data
+
+
