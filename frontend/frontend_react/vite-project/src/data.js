8308fc9947431ac2e7c3d3f69aeb41e0dccc01d4
mikiiiss <mihreykiros23@gmail.com>
2024-05-24 17:05:18 +0300

    frontend

---
+++ frontend/frontend_react/vite-project/src/data.js
@@ -0,0 +1,206 @@
+export const data = [{
+    "id": 1,
+    "image_url": "ut erat id mauris vulputate elementum nullam varius nulla facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt",
+    "title": "in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio justo sollicitudin ut",
+    "article_url": "http://java.",
+    "highlight": "est lacinia nisi venenatis tristique fusce congue diam id ornare imperdiet sapien urna pretium nisl ut volutpat sapien arcu",
+    "time_publish": "ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis",
+    "category": "habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque",
+    "date_published": 48,
+    "publisher_name": "tempor convallis nulla neque libero convallis eget eleifend luctus ultricies",
+    "detail_content": "tempor convallis nulla neque libero convallis eget eleifend luctus ultricies"
+
+}
+//   }, {
+//     "_id": 2,
+//     "url": "placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget",
+//     "source": "https://about.",
+//     "title": "a nibh in quis justo maecenas rhoncus aliquam lacus morbi quis tortor id nulla ultrices aliquet",
+//     "text": "turpis adipiscing lorem vitae mattis nibh ligula nec sem duis aliquam convallis nunc proin at",
+//     "category": "egestas metus aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero convallis",
+//     "tags": "diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis fusce",
+//     "word_count": 49,
+//     "language": "curae nulla dapibus dolor vel est donec odio justo sollicitudin ut suscipit a feugiat et eros vestibulum ac est"
+//   }, {
+//     "_id": 3,
+//     "url": "erat fermentum justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque",
+//     "source": "http://usatoday.",
+//     "title": "orci pede venenatis non sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus",
+//     "text": "velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque",
+//     "category": "lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed",
+//     "tags": "lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse",
+//     "word_count": 73,
+//     "language": "venenatis non sodales sed tincidunt eu felis fusce posuere felis sed"
+//   }, {
+//     "_id": 4,
+//     "url": "ultrices mattis odio donec vitae nisi nam ultrices libero non",
+//     "source": "http://people.com.",
+//     "title": "quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse platea dictumst maecenas ut",
+//     "text": "sed magna at nunc commodo placerat praesent blandit nam nulla integer",
+//     "category": "amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti",
+//     "tags": "nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem",
+//     "word_count": 97,
+//     "language": "in eleifend quam a odio in hac habitasse platea dictumst maecenas ut massa"
+//   }, {
+//     "_id": 5,
+//     "url": "tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo placerat praesent blandit",
+//     "source": "https://altervista",
+//     "title": "sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis",
+//     "text": "nulla nunc purus phasellus in felis donec semper sapien a libero nam",
+//     "category": "purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient",
+//     "tags": "primis in faucibus orci luctus et ultrices posuere cubilia curae nulla",
+//     "word_count": 34,
+//     "language": "aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur"
+//   }, {
+//     "_id": 6,
+//     "url": "rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis ligula",
+//     "source": "https://unesco.org",
+//     "title": "leo odio condimentum id luctus nec molestie sed justo pellentesque viverra pede",
+//     "text": "mauris vulputate elementum nullam varius nulla facilisi cras non velit nec nisi vulputate nonummy maecenas",
+//     "category": "vel accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula",
+//     "tags": "pellentesque viverra pede ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper est quam",
+//     "word_count": 28,
+//     "language": "sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc"
+//   }, {
+//     "_id": 7,
+//     "url": "vitae nisi nam ultrices libero non mattis pulvinar nulla pede ullamcorper augue a suscipit nulla elit ac nulla",
+//     "source": "https://nifty.com/",
+//     "title": "lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque",
+//     "text": "suspendisse ornare consequat lectus in est risus auctor sed tristique in",
+//     "category": "faucibus orci luctus et ultrices posuere cubilia curae duis faucibus accumsan odio curabitur convallis duis consequat dui",
+//     "tags": "curae nulla dapibus dolor vel est donec odio justo sollicitudin ut suscipit a feugiat et eros vestibulum ac est",
+//     "word_count": 25,
+//     "language": "erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus"
+//   }, {
+//     "_id": 8,
+//     "url": "ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere",
+//     "source": "https://vistaprint.",
+//     "title": "in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis",
+//     "text": "cum sociis natoque penatibus et magnis dis parturient montes nascetur",
+//     "category": "rutrum nulla tellus in sagittis dui vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium",
+//     "tags": "nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa",
+//     "word_count": 85,
+//     "language": "nunc commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel pede"
+//   }, {
+//     "_id": 9,
+//     "url": "ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac",
+//     "source": "https://go.com/odio",
+//     "title": "penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus",
+//     "text": "nec molestie sed justo pellentesque viverra pede ac diam cras pellentesque volutpat",
+//     "category": "massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac habitasse",
+//     "tags": "sem sed sagittis nam congue risus semper porta volutpat quam",
+//     "word_count": 24,
+//     "language": "id massa id nisl venenatis lacinia aenean sit amet justo morbi ut odio cras mi pede malesuada"
+//   }, {
+//     "_id": 10,
+//     "url": "in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia",
+//     "source": "https://china.com.cn/",
+//     "title": "nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta",
+//     "text": "ut erat id mauris vulputate elementum nullam varius nulla facilisi cras non velit",
+//     "category": "erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus",
+//     "tags": "interdum mauris ullamcorper purus sit amet nulla quisque arcu libero rutrum ac lobortis vel dapibus at",
+//     "word_count": 35,
+//     "language": "amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum"
+//   }, {
+//     "_id": 11,
+//     "url": "nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere",
+//     "source": "https://cisco.com/ipsum",
+//     "title": "congue diam id ornare imperdiet sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam",
+//     "text": "tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut",
+//     "category": "erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis donec semper sapien a",
+//     "tags": "nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in",
+//     "word_count": 2,
+//     "language": "ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper est quam pharetra magna ac"
+//   }, {
+//     "_id": 12,
+//     "url": "imperdiet nullam orci pede venenatis non sodales sed tincidunt eu",
+//     "source": "https://devhub.com/primis",
+//     "title": "maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros",
+//     "text": "sit amet nulla quisque arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu pede",
+//     "category": "in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis",
+//     "tags": "quam pharetra magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci",
+//     "word_count": 43,
+//     "language": "suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam"
+//   }, {
+//     "_id": 13,
+//     "url": "cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam",
+//     "source": "https://sakura.ne.jp/justo",
+//     "title": "lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa volutpat convallis morbi",
+//     "text": "erat id mauris vulputate elementum nullam varius nulla facilisi cras",
+//     "category": "vestibulum ac est lacinia nisi venenatis tristique fusce congue diam id ornare",
+//     "tags": "montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et",
+//     "word_count": 64,
+//     "language": "in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum"
+//   }, {
+//     "_id": 14,
+//     "url": "est phasellus sit amet erat nulla tempus vivamus in felis eu sapien",
+//     "source": "http://newsvine.com/lorem",
+//     "title": "et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida",
+//     "text": "eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus",
+//     "category": "cursus urna ut tellus nulla ut erat id mauris vulputate elementum nullam varius nulla facilisi cras non velit",
+//     "tags": "at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse",
+//     "word_count": 7,
+//     "language": "lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat"
+//   }, {
+//     "_id": 15,
+//     "url": "vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus dolor vel est",
+//     "source": "http://timesonline.co.uk/",
+//     "title": "at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at",
+//     "text": "sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla",
+//     "category": "duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam",
+//     "tags": "justo sollicitudin ut suscipit a feugiat et eros vestibulum ac est lacinia nisi venenatis tristique fusce",
+//     "word_count": 81,
+//     "language": "cubilia curae duis faucibus accumsan odio curabitur convallis duis consequat dui nec"
+//   }, {
+//     "_id": 16,
+//     "url": "orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula",
+//     "source": "https://amazon.co.",
+//     "title": "gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum",
+//     "text": "sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum",
+//     "category": "nec molestie sed justo pellentesque viverra pede ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper",
+//     "tags": "aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in",
+//     "word_count": 79,
+//     "language": "nam congue risus semper porta volutpat quam pede lobortis ligula sit amet"
+//   }, {
+//     "_id": 17,
+//     "url": "ut at dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae",
+//     "source": "http://census.gov/rutrum/rutrum/",
+//     "title": "eget rutrum at lorem integer tincidunt ante vel ipsum praesent",
+//     "text": "proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque",
+//     "category": "dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent",
+//     "tags": "et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida",
+//     "word_count": 27,
+//     "language": "lacus curabitur at ipsum ac tellus semper interdum mauris ullamcorper purus sit"
+//   }, {
+//     "_id": 18,
+//     "url": "nibh ligula nec sem duis aliquam convallis nunc proin at turpis a pede posuere nonummy integer non velit donec",
+//     "source": "https://biglobe.ne.jp/porttitor/",
+//     "title": "adipiscing elit proin risus praesent lectus vestibulum quam sapien varius ut blandit",
+//     "text": "sem duis aliquam convallis nunc proin at turpis a pede",
+//     "category": "neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl duis ac nibh",
+//     "tags": "sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor",
+//     "word_count": 76,
+//     "language": "elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue"
+//   }, {
+//     "_id": 19,
+//     "url": "aliquet massa id lobortis convallis tortor risus dapibus augue vel",
+//     "source": "https://blogspot.comallis=in&nulla=quam&neque=fringilla",
+//     "title": "tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis",
+//     "text": "posuere cubilia curae donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet",
+//     "category": "hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer",
+//     "tags": "aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non",
+//     "word_count": 29,
+//     "language": "massa donec dapibus duis at velit eu est congue elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium"
+//   }, {
+//     "_id": 20,
+//     "url": "dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus",
+//     "source": "https://diigo.com/",
+//     "title": "nisi nam ultrices libero non mattis pulvinar nulla pede ullamcorper",
+//     "text": "sollicitudin ut suscipit a feugiat et eros vestibulum ac est lacinia nisi venenatis tristique fusce congue diam id ornare",
+//     "category": "id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus",
+//     "tags": "ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque arcu libero rutrum",
+//     "word_count": 99,
+//     "language": "luctus nec molestie sed justo pellentesque viverra pede ac diam cras pellentesque volutpat"
+//   }
+]
+  
